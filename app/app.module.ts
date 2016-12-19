b2b5864b311b1d8d7c36cadabe07dc7a4f276e2a
Adam Pasternack <adam.pasternack@gmail.com>
2016-12-18 20:52:57 -0500

    pipes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component';
 
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent,
-  ProductListComponent ],
+  imports: [
+    BrowserModule,
+    FormsModule // added to accomodate use of ngModel for two way binding
+  ],
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
