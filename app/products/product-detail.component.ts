5b65d47a9ded077de88c3036ef15cd40ab4ec8c4
Adam Pasternack <adam.pasternack@gmail.com>
2017-02-21 11:06:27 -0500

    custom routing

--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -10,7 +10,8 @@ export class ProductDetailComponent implements OnInit {
     pageTitle: string = 'Product Detail';
     product: IProduct;
 
-    constructor(private _route: ActivatedRoute) {
+    constructor(private _route: ActivatedRoute,
+                private _router: Router) {
 
     }
 
@@ -18,6 +19,10 @@ export class ProductDetailComponent implements OnInit {
         let id = +this._route.snapshot.params['id'];
         this.pageTitle += `: ${id}`;
         
+    }
+
+    onBack(): void {
+        this._router.navigate(['/products']);
     }
 
 
