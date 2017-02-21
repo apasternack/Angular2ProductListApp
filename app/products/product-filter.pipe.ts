3d8f2859126749aaa6f7efcd42c4008a0d13e0e9
Adam Pasternack <adam.pasternack@gmail.com>
2017-02-21 09:51:04 -0500

    filter

--- app/products/product-filter.pipe.ts
+++ app/products/product-filter.pipe.ts
@@ -11,7 +11,9 @@ export class ProductFilterPipe implements PipeTransform {
     transform(value: IProduct[], filterBy: string): IProduct[] {
 
         filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
+        
         return filterBy ? value.filter((product: IProduct) =>
+        
         product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
 
     }
