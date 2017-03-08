14e61ec3f994835aa4f0c646cdc69ae93a5a04a5
Adam Pasternack <adam.pasternack@gmail.com>
2017-03-07 21:36:05 -0500

    shared module

--- app/products/product.module.ts
+++ app/products/product.module.ts
@@ -1,29 +1,23 @@
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
 
 import { ProductDetailGuard } from './product-guard.service';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductListComponent } from './product-list.component';
 import { ProductService } from './product.service';
-import { StarComponent } from '../shared/star.component';
 
-
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
-        ProductFilterPipe,
-        StarComponent
+        ProductFilterPipe
     ],
     imports: [
-        FormsModule,   // added to accomodate use of ngModel for two way binding
-        CommonModule,
+        SharedModule,
         RouterModule.forChild([
                   { path: 'products', component: ProductListComponent },
       { path: 'product/:id',
