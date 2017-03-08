a4ea6fc4fa05f45a3e8a9f6c22b69cdae77ea50d
Adam Pasternack <adam.pasternack@gmail.com>
2017-03-07 21:47:07 -0500

    shared

--- app/products/product.module.js
+++ app/products/product.module.js
@@ -5,8 +5,6 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var common_1 = require("@angular/common");
-var forms_1 = require("@angular/forms");
 var core_1 = require("@angular/core");
 var router_1 = require("@angular/router");
 var product_guard_service_1 = require("./product-guard.service");
@@ -14,7 +12,7 @@ var product_detail_component_1 = require("./product-detail.component");
 var product_filter_pipe_1 = require("./product-filter.pipe");
 var product_list_component_1 = require("./product-list.component");
 var product_service_1 = require("./product.service");
-var star_component_1 = require("../shared/star.component");
+var shared_module_1 = require("../shared/shared.module");
 var ProductModule = (function () {
     function ProductModule() {
     }
@@ -25,12 +23,10 @@ ProductModule = __decorate([
         declarations: [
             product_list_component_1.ProductListComponent,
             product_detail_component_1.ProductDetailComponent,
-            product_filter_pipe_1.ProductFilterPipe,
-            star_component_1.StarComponent
+            product_filter_pipe_1.ProductFilterPipe
         ],
         imports: [
-            forms_1.FormsModule,
-            common_1.CommonModule,
+            shared_module_1.SharedModule,
             router_1.RouterModule.forChild([
                 { path: 'products', component: product_list_component_1.ProductListComponent },
                 { path: 'product/:id',
